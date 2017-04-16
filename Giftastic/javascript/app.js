0a45ed0fb82f5041e936305a92a2ec759d9a1d56
sallynasrfields <sally.nasr@gmail.com>
2017-04-16 17:48:49 -0500

    Continuing to troubleshoot Security issue

--- Giftastic/javascript/app.js
+++ Giftastic/javascript/app.js
@@ -18,7 +18,7 @@ $(document).ready(function()
 
 	$(document).off('click','.animalButton').on('click', '.animalButton',function() {
 		var animal = (this.innerHTML);
-		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
         animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
         $.ajax({
