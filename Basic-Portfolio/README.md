d1271ffdaf42434a13d6f33b1b323bb99348964e
Sally N Fields <sally.nasr@gmail.com>
2017-11-25 14:34:43 -0600

    repo restructure

--- Basic-Portfolio/README.md
+++
@@ -1,2 +0,0 @@
-# Basic-Portfolio
-Sally Fields's Basic Portfolio
--- LIRI-Bot/keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys =  {
-  consumer_key: 'RIsNXHzQ67EG6QTl4IZDy3wqz',
-  consumer_secret: 'lDDYN2ZPo5LYUNW4IVEhEKasmgsBr4U2TI0bEp1pgnHkukoM3v',
-  access_token_key: '271166052-iYjsvXWIYKeNxkq5UFI6zGkPgI7mzzCMiHlwOttn',
-  access_token_secret: '5o3uaiFKM7TWKETjDwEe5iFCpipvXSnnLzgZsRh78TpdR',
-}
